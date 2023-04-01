8f5b614e5594e55ce796a51a7a3faa5ee04577fc
casper-fernandes <57257377+casper-fernandes@users.noreply.github.com>
2023-04-01 13:25:16 +0530

    Provider wrapped

--- src/index.js
+++ src/index.js
@@ -3,10 +3,14 @@ import ReactDOM from 'react-dom/client';
 
 import App from './App';
 
+import AppProvider from './appContext/AppProvider';
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
   <React.StrictMode>
-    <App />
+    <AppProvider>
+      <App />
+    </AppProvider>
   </React.StrictMode>
 );
